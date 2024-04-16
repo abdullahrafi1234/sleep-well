caf200c29ec81e456132ebae53fc5dcb302957c0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-04-17 00:02:09 +0600

    added router

---
+++ src/pages/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3>This is Navbar</h3>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,19 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Root from "../pages/Root/Root";
+import Home from "../pages/Home/Home";
 
   const Router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
     },
   ]);
 
